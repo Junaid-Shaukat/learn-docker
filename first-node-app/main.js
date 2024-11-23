c4189c7597fdfa2dc276d17ba1a4a78010988ebf
Junaid-Shaukat <junaiddshaukat@gmail.com>
2024-11-23 18:26:39 +0500

    feat: add .dockerignore, set working directory in Dockerfile, and update server port in main.js

--- first-node-app/main.js
+++ first-node-app/main.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 app.get("/",(req,res)=>{
     res.send("<h1>Hello from docker node js project</h1>")
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
+app.listen(PORT,()=>{
+    console.log("Server is running on port ,",PORT)
 })
